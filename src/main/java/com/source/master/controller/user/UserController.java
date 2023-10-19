eb7a2b3170f97aba99821aedac7b77183dd8bc10
Rabendra <rabendra@tracecost.com>
2023-10-19 21:20:52 +0530

    Fix

--- src/main/java/com/source/master/controller/user/UserController.java
+++ src/main/java/com/source/master/controller/user/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
